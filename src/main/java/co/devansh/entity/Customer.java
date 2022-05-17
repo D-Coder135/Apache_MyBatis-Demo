6cfa1f09ab02aad48f305e12681ba8125de226e6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-17 22:26:22 +0530

    add the @ToString annotation for the toString() methods

--- src/main/java/co/devansh/entity/Customer.java
+++ src/main/java/co/devansh/entity/Customer.java
@@ -12,6 +12,7 @@ package co.devansh.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Objects;
 
@@ -19,6 +20,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Customer {
     private Integer id;
     private String name;
