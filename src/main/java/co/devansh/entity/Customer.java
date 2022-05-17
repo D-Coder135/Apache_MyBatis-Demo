20b815fe52064590fa76a496c317ffc604839708
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-17 22:13:28 +0530

    add a new java class Customer.java inside the package co.devansh.entity

---
+++ src/main/java/co/devansh/entity/Customer.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 17-05-2022/05/2022
+ *   Time: 10:13 PM
+ *   File: Customer
+ */
+
+package co.devansh.entity;
+
+public class Customer {
+}
