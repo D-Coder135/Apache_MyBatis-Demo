ec624456da1f0045ca390382750245990d6675bb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-17 22:37:07 +0530

    add an abstract method addCustomer() of type void inside the interface CustomerDao.java

--- src/main/java/co/devansh/dao/CustomerDao.java
+++ src/main/java/co/devansh/dao/CustomerDao.java
@@ -1,7 +1,11 @@
 package co.devansh.dao;
 
+import co.devansh.entity.Customer;
+
 public interface CustomerDao {
     // CRUD OPERATIONS
+
+    public void addCustomer(Customer customer);
 
     // QUERIES
 }
