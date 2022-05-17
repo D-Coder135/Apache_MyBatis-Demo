557c8c72f2049915561e92f28ecd444810e0e109
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-17 22:38:45 +0530

    add an @Insert annotation along with a sql query inside it

--- src/main/java/co/devansh/dao/CustomerDao.java
+++ src/main/java/co/devansh/dao/CustomerDao.java
@@ -1,10 +1,12 @@
 package co.devansh.dao;
 
 import co.devansh.entity.Customer;
+import org.apache.ibatis.annotations.Insert;
 
 public interface CustomerDao {
     // CRUD OPERATIONS
 
+    @Insert("insert into customer (name, city, email, phone) values (#{name}, #{city}, #{email}, #{phone})")
     public void addCustomer(Customer customer);
 
     // QUERIES
