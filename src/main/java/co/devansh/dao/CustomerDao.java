0765532fcaf8510fef8113eb47add61ec4a284d9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-13 23:49:41 +0530

    add a public abstract method of type List<Customer> getCustomerByCity()

--- src/main/java/co/devansh/dao/CustomerDao.java
+++ src/main/java/co/devansh/dao/CustomerDao.java
@@ -27,4 +27,6 @@ public interface CustomerDao {
 
     @Select("select * from customer")
     public List<Customer> getAllCustomers();
+
+    public List<Customer> getCustomerByCity(String city);
 }
