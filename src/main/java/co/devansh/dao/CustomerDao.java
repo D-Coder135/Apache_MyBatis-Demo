0d3bf57314e9d8db7a0166e855acf04d560d8b03
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-13 23:47:01 +0530

    add the @Delete annotation along with the required SQL query

--- src/main/java/co/devansh/dao/CustomerDao.java
+++ src/main/java/co/devansh/dao/CustomerDao.java
@@ -1,6 +1,7 @@
 package co.devansh.dao;
 
 import co.devansh.entity.Customer;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -17,6 +18,7 @@ public interface CustomerDao {
     @Update("update customer set name=#{name}, city=#{city}, email=#{email}, phone=#{phone} where id=#{id}")
     public void updateCustomer(Customer customer);
 
+    @Delete("delete from customer where id=#{id}")
     public void deleteCustomer(Integer id);
     // QUERIES
 }
