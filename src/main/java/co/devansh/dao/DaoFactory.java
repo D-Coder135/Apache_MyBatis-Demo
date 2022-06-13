8775b32a8b45fda15223e79375c4fac7520ba63f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-14 00:04:08 +0530

    call the Resources.getResourceAsStream() method

--- src/main/java/co/devansh/dao/DaoFactory.java
+++ src/main/java/co/devansh/dao/DaoFactory.java
@@ -7,12 +7,14 @@
 
 package co.devansh.dao;
 
+import org.apache.ibatis.io.Resources;
+
 public final class DaoFactory {
 
     private DaoFactory() {
     }
 
     public static CustomerDao getCustomerDao() {
-
+        Resources.getResourceAsStream("mybatis-config.xml");
     }
 }
