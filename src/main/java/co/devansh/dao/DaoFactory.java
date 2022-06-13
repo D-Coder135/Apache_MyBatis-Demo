65423370f6c148ad640e389354b44093050778d3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-14 00:00:31 +0530

    add a new class DaoFactory.java inside the package co.devansh.dao

---
+++ src/main/java/co/devansh/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 14-06-2022/06/2022
+ *   Time: 12:00 AM
+ *   File: DaoFactory
+ */
+
+package co.devansh.dao;
+
+public class DaoFactory {
+}
