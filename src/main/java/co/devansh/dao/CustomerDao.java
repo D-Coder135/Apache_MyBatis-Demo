94292585872ebdd3b8098295d4124399d4380db1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-13 23:45:29 +0530

    add the @Update annotation along with the required SQL query

--- src/main/java/co/devansh/dao/CustomerDao.java
+++ src/main/java/co/devansh/dao/CustomerDao.java
@@ -3,6 +3,7 @@ package co.devansh.dao;
 import co.devansh.entity.Customer;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 public interface CustomerDao {
     // CRUD OPERATIONS
@@ -13,6 +14,7 @@ public interface CustomerDao {
     @Select("select * from customer where id=#{id}")
     public Customer getCustomerById(Integer id);
 
+    @Update("update customer set name=#{name}, city=#{city}, email=#{email}, phone=#{phone} where id=#{id}")
     public void updateCustomer(Customer customer);
     // QUERIES
 }
