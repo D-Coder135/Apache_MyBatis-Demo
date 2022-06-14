decf64a2e034b5020a02576f043a6c280047084e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-15 00:20:18 +0530

    add a new class RetrieveCustomerById.java inside the package co.devansh.programs

---
+++ src/main/java/co/devansh/programs/RetrieveCustomerById.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 15-06-2022/06/2022
+ *   Time: 12:20 AM
+ *   File: RetrieveCustomerById
+ */
+
+package co.devansh.programs;
+
+public class RetrieveCustomerById {
+}
