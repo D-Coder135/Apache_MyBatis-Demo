d42fe624061abfcdef2a596cadbb496a269b528d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-15 13:08:04 +0530

    paste some code from class RetrieveCustomerById.java class

--- src/main/java/co/devansh/programs/UpdateCustomerData.java
+++ src/main/java/co/devansh/programs/UpdateCustomerData.java
@@ -9,11 +9,21 @@ package co.devansh.programs;
 
 import co.devansh.dao.CustomerDao;
 import co.devansh.dao.DaoFactory;
+import co.devansh.entity.Customer;
 
 import java.io.IOException;
 
 public class UpdateCustomerData {
     public static void main(String[] args) throws IOException {
         CustomerDao dao = DaoFactory.getCustomerDao();
+
+        int id = 2;
+        Customer c1 = dao.getCustomerById(id);
+
+        if (c1 == null) {
+            System.out.println("No customer data for id: " + id);
+        } else {
+            System.out.println(c1);
+        }
     }
 }
