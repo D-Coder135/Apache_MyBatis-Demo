4342601d34e1188e3e16b2c57ef42db7f5f54eb0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-15 13:54:44 +0530

    initialize a field of type List<Customer>

--- src/main/java/co/devansh/programs/GetCustomersByCity.java
+++ src/main/java/co/devansh/programs/GetCustomersByCity.java
@@ -9,13 +9,16 @@ package co.devansh.programs;
 
 import co.devansh.dao.CustomerDao;
 import co.devansh.dao.DaoFactory;
+import co.devansh.entity.Customer;
 
 import java.io.IOException;
+import java.util.List;
 
 public class GetCustomersByCity {
     public static void main(String[] args) throws IOException {
         CustomerDao dao = DaoFactory.getCustomerDao();
 
         String city = "Varanasi";
+        List<Customer> list = dao.getCustomerByCity(city);
     }
 }
