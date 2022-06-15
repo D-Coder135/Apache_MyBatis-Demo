a29159de60d0cbccfa8e121add0f817241e4ebf2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-15 13:50:35 +0530

    add a new class GetCustomersByCity.java inside the package co.devansh.programs

---
+++ src/main/java/co/devansh/programs/GetCustomersByCity.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 15-06-2022/06/2022
+ *   Time: 01:50 PM
+ *   File: GetCustomersByCity
+ */
+
+package co.devansh.programs;
+
+public class GetCustomersByCity {
+}
