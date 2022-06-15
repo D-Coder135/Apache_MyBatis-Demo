f7719af98090950212bde0019037686dffb619c1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-15 13:18:13 +0530

    add a new class DeleteCustomer.java inside the package co.devansh.programs

---
+++ src/main/java/co/devansh/programs/DeleteCustomer.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 15-06-2022/06/2022
+ *   Time: 01:17 PM
+ *   File: DeleteCustomer
+ */
+
+package co.devansh.programs;
+
+public class DeleteCustomer {
+}
